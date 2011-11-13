d123756908c7f4dbdd6e621c2dbbb83c849f3b19
Martin Kozák <martinkozak@martinkozak.net>
2011-11-13 01:32:08 +0100

    minor correction

--- build/minimal/makearchec2.sh
+++ build/minimal/makearchec2.sh
@@ -186,8 +186,7 @@ curl -o $ROOT/root/repo/ec2-metadata-0.1-1-any.pkg.tar.xz https://raw.github.com
 curl -o $ROOT/root/repo/ec2arch-1.0-1-any.pkg.tar.xz https://raw.github.com/martinkozak/ec2build/master/kernel/repo/ec2arch-1.0-1-any.pkg.tar.xz
 curl -o $ROOT/root/repo/linux-ec2-3.1-4-$ARCH.pkg.tar.xz http://c263555.r55.cf1.rackcdn.com/linux-ec2-3.1-4-$ARCH.pkg.tar.xz
 curl -o $ROOT/root/repo/linux-ec2-headers-3.1-4-$ARCH.pkg.tar.xz http://c263555.r55.cf1.rackcdn.com/linux-ec2-headers-3.1-4-$ARCH.pkg.tar.xz
-repo-add /root/repo/ec2.db.tar.gz /root/repo/ec2arch-1.0-1-any.pkg.tar.xz /root/repo/ec2-metadata-0.1-1-any.pkg.tar.xz linux-ec2-3.1-4-$ARCH.pkg.tar.xz linux-ec2-headers-3.1-4-$ARCH.pkg.tar.xz
-
+repo-add /root/repo/ec2.db.tar.gz /root/repo/ec2arch-1.0-1-any.pkg.tar.xz /root/repo/ec2-metadata-0.1-1-any.pkg.tar.xz /root/repo/linux-ec2-3.1-4-$ARCH.pkg.tar.xz /root/repo/linux-ec2-headers-3.1-4-$ARCH.pkg.tar.xz
 
 cd $ROOT
 find . -depth -print | cpio -pdmv --sparse $NEWROOT

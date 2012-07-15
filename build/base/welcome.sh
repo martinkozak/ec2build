5e44ad8a9273421999c157a7fd3afb340031ee84
Martin Kozák <martinkozak@martinkozak.net>
2012-07-16 00:00:31 +0200

    welcome script

---
+++ build/base/welcome.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo "
+######################################################################
+
+WELCOME to Arch Linux on Amazon EC2! It's a standard (base) 
+system with very reduced patched standard kernel suitable for EC2.
+
+For the latest EC2 enabled kernel or other EC2 related tools updates, 
+you can perform standard \"pacman -Syu\" because it's connected to 
+the binary precompiled archlinux-ec2 repository.
+
+General support for this images and home of this project are at: 
+    https://github.com/martinkozak/ec2build
+    https://github.com/martinkozak/linux-ec2
+
+You probably should initialize the archlinux-keyring now by:
+    pacman-key --init; pacman-key --populate archlinux
+    
+You can disable this message by deleting:
+    /etc/ec2/welcome.sh
+    
+######################################################################
+"
+
+uname -a
+echo
+echo

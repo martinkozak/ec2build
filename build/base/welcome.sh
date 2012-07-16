e1d98a62a329c93ca13e897068a95899fa388906
Martin Kozák <martinkozak@martinkozak.net>
2012-07-16 10:55:32 +0200

    style and frammar corrections of the welcome text

--- build/base/welcome.sh
+++ build/base/welcome.sh
@@ -6,25 +6,26 @@ echo "
 WELCOME to Arch Linux on Amazon EC2! It's a standard (base) 
 system with very reduced patched standard kernel suitable for EC2.
 
-For the latest EC2 enabled kernel or other EC2 related tools updates, 
+For the latest EC2 enabled kernel or other EC2 related tools updates,
 you can perform standard \"pacman -Syu\" because it's connected to 
 the binary precompiled archlinux-ec2 repository.
 
-General support for this images and home of this project are at: 
+General support for these images and home of this project are at: 
     https://github.com/martinkozak/ec2build
     https://github.com/martinkozak/linux-ec2
 
-You probably should initialize the archlinux-keyring now by:
+You probably should initialize the archlinux-keyring now by running:
     pacman-key --init; pacman-key --populate archlinux
     
 You can disable this message by removing the file:
     /etc/ec2/welcome.sh
     
+Enjoy the administration!
+    
 ######################################################################
 "
 
 uname -a
-echo
-echo "Version 20120716."
+echo "Image version 20120716."
 echo
 echo
